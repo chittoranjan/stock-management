c1c705d663cf7c0ef71140e76564e7d2338e31a6
chittoranjan <chittoranjan.sarker@gmail.com>
2018-05-10 16:04:18 +0600

    re-install jquery and unuptosive js file

--- StockManagement.Models/EntityModels/Category.cs
+++ StockManagement.Models/EntityModels/Category.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +11,14 @@ namespace StockManagement.Models.EntityModels
     public class Category
     {
         public int Id { get; set; }
+
+        //[Required(ErrorMessage = "Please write Category Name")]
+        [Display(Name = "Category Name")]
         public string Name { get; set; }
+
+        //[Required(ErrorMessage = "Please write a code 3-6 digits")]
+        [Display(Name = "Category Code")]
+        //[MinLength(3 , ErrorMessage = "Please write minimum 3 digits"),MaxLength(6, ErrorMessage = "Please write maximum 6 digits")]
         public string Code { get; set; }
 
         public List<Product> Products { get; set; }
