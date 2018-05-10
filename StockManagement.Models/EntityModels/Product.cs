57e4b687615b28ffb2f942dfbe327baa8a75a3f9
chittoranjan <chittoranjan.sarker@gmail.com>
2018-05-10 19:16:59 +0600

    css added in product page

--- StockManagement.Models/EntityModels/Product.cs
+++ StockManagement.Models/EntityModels/Product.cs
@@ -1,11 +1,20 @@
-﻿namespace StockManagement.Models.EntityModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockManagement.Models.EntityModels
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Display(Name = "Product Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Product Code")]
         public string Code { get; set; }
+
+        [Display(Name = "Category Name")]
         public int CategoryId { get; set; }
+        
         public Category Category { get; set; }
     }
 }
